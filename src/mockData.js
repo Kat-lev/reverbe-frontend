a200c7f9d9f5a51ca2605f2b5c446698100af6d9
kat-lev <kat@katleverton.com>
2025-10-22 16:44:44 +0200

    Initial commit: Create project in React + Vite, copy mockData placeholder and visualise data in App.js

---
+++ src/mockData.js
@@ -0,0 +1,87 @@
+export const mockMessages = [
+  {
+    id: "f8c0cccc-ffc0-48f9-b119-f85c0f7cffea",
+    assumpte: '[Diari] "Ram"',
+    remitent: "diari@raio.issim.net",
+    cos: `Assumpte: Ram
+
+Missatge:
+El joc és la millor gimnàsia
+
+Adjunts:
+
+_______________________________________________Llista de correu Diari -- diari@raio.issim.net Per donar-se de baixa envieu un correu a diari-leave@raio.issim.net`,
+    data: "2025-06-11T11:13:30",
+    reverberacions: [
+      {
+        id: "b2e86d28-4b14-4739-a469-11844db94e2d",
+        missatge_original_id: "f8c0cccc-ffc0-48f9-b119-f85c0f7cffea",
+        assumpte: '[Futurs] Re: [Diari] "Ram"',
+        remitent: "futurs@raio.issim.net",
+        cos: `i el futur és una festa
+
+> On 17 Jun 2025, at 20:27, central@mail.issim.net wrote:
+>
+> Assumpte: Ram
+>
+> Missatge:
+> El joc és la millor gimnàsia
+>
+> Adjunts:
+>
+> _______________________________________________Llista de correu Diari -- diari@raio.issim.net Per donar-se de baixa envieu un correu a diari-leave@raio.issim.net
+>
+> [centraleta-id: f8c0cccc-ffc0-48f9-b119-f85c0f7cffea]`,
+        data: "2025-06-17T20:46:32",
+      },
+    ],
+  },
+  {
+    id: "81410a66-e2cd-4e37-bb2d-209c205e0eff",
+    assumpte: '[Diari] "Recuerdo recuerdo"',
+    remitent: "diari@raio.issim.net",
+    cos: `Assumpte: Recuerdo recuerdo
+
+Missatge:
+Un día de invierno pero soleado, frío pero cálido, dentro de la institución pero fuera de ella, escuchando pero ensimismando, hablando pero callando. Nos recuerdo en la terraza pero inclinada, en el conflicto pero en el estar juntas. Los demás pero yo, yo pero los demás, tu pero yo, yo pero tu. El dinero pero la responsabilidad, la responsabilidad pero la vida, la vida pero el tiempo, el tiempo pero los plazos, los plazos pero el privilegio.
+
+Adjunts:
+
+_______________________________________________Llista de correu Diari -- diari@raio.issim.net Per donar-se de baixa envieu un correu a diari-leave@raio.issim.net`,
+    data: "2025-06-19T08:10:40",
+    reverberacions: [
+      {
+        id: "563d84ae-5ab5-45e0-9c36-a2e92bbb740f",
+        missatge_original_id: "81410a66-e2cd-4e37-bb2d-209c205e0eff",
+        assumpte: '[Ihopethismessagefindsyouwell] Re: [Diari] "Recuerdo recuerdo"',
+        remitent: "ihopethismessagefindsyouwell@raio.issim.net",
+        cos: `￼                       _  _  _  _ _   _ _ _ //)(-_) _) (-/)(/(-/                _/      > El 19 juny 2025, a les 11:03, central@mail.issim.net va escriure:> > Assumpte: Recuerdo recuerdo> > Missatge:> Un día de invierno pero soleado, frío pero cálido, dentro de la institución pero fuera de ella, escuchando pero ensimismando, hablando pero callando. Nos recuerdo en la terraza pero inclinada, en el conflicto pero en el estar juntas. Los demás pero yo, yo pero los demás, tu pero yo, yo pero tu. El dinero pero la responsabilidad, la responsabilidad pero la vida, la vida pero el tiempo, el tiempo pero los plazos, los plazos pero el privilegio.> > Adjunts:> > _______________________________________________Llista de correu Diari -- diari@raio.issim.net Per donar-se de baixa envieu un correu a diari-leave@raio.issim.net> > > [centraleta-id: 81410a66-e2cd-4e37-bb2d-209c205e0eff]`,
+        data: "2025-06-20T13:10:44",
+      },
+    ],
+  },
+  {
+    id: "b7a0d472-f942-4c14-b711-6551f41b09b8",
+    assumpte: '[Diari] "Sobre la dificultad de recordar un jueves por la mañana"',
+    remitent: "diari@raio.issim.net",
+    cos: `Assumpte: Sobre la dificultad de recordar un jueves por la mañana
+
+Missatge:
+Cuando la disposición del tiempo es espiralado, aka, desde hace unos meses en los que el vértice, aka, lo que mueve sin moverse fuera pero dentro del propio cuerpo, succiona con más fuerza que nunca. Digo, cuando el tiempo gira sobre sí mismo mientras desplaza su caída, los recuerdos salen despedidos con una velocidad lateral extremada que, junto con el roce prefrontal, los inflama a su paso por la carne. Vacío de recuerdos. Lleno de una sustancia temporal altamente pegajosa.
+
+Adjunts:
+
+_______________________________________________Llista de correu Diari -- diari@raio.issim.net Per donar-se de baixa envieu un correu a diari-leave@raio.issim.net`,
+    data: "2025-06-19T08:06:41",
+    reverberacions: [
+      {
+        id: "d1d1d103-9aa4-451f-aea4-e975ced495e2",
+        missatge_original_id: "b7a0d472-f942-4c14-b711-6551f41b09b8",
+        assumpte: '[Futurs] Re: [Diari] "Sobre la dificultad de recordar un jueves por la mañana"',
+        remitent: "futurs@raio.issim.net",
+        cos: `Els dibuts són una societat peculiar que presenta certes diferències anatòmiques destacables respecte als humans. El més cridaner, i el que ha capturat la imaginació de tants infants humans al llarg dels anys, és que semblen no tenir cap. En realitat sí que en tenen, però el tenen dins del cos, replegat com si estigués protegit per una vaina. Els dibuts són individus corpulents, i el seu cap anatòmic s’ubica rere la part superior de l’estèrnum. Del que els dibuts no disposen és de boca, tot i que sí que hi veuen i hi senten a través d’unes mucoses situades allà on un humà tindria el coll. El fet de conformar una societat muda ha permès als dibuts desenvolupar una altra manera de comunicar-se, que fa ús de la rotació dels braços i del moviment del tronc. Probablement degut a la seva menor capacitat ocular i a la peculiar posició del seu cap dins el tors, els moviments amb què es comuniquen els dibuts sempre són amplis i contundents, la qual cosa fa que els seus edificis resultin monumentals a ulls humans: els cal molt d’espai per dir-se el que es volen dir._______________________________________________Futurs mailing list -- futurs@raio.issim.net To unsubscribe send an email to futurs-leave@raio.issim.net`,
+        data: "2025-06-24T23:52:11",
+      },
+    ],
+  },
+];
