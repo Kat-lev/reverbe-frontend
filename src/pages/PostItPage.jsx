567a0bd9f4aa45ec08d5ed65eafe58a9d306c0c6
Mariona Cuyàs <cuyas.mariona@egmail.com>
2025-10-29 17:54:03 +0100

    Add style to post it page

--- src/pages/PostItPage.jsx
+++ src/pages/PostItPage.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../components/Header";
 import Card from "../components/Card";
+import Modal from "../components/Modal";
 import { mockMessages } from "../mockData";
 
 function PostItPage() {
+  const [selected, setSelected] = useState(null);
+
   return (
   <div className="min-h-screen bg-gray-100">
     <Header />
     <main
         className="
-          max-w-7xl mx-auto 
+          max-w-7xl mx-auto mt-14
           p-4 sm:p-6 lg:p-8 
           grid gap-6
           grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
         "
       >
         {mockMessages.slice(0, 6).map((msg) => (
-          <Card key={msg.id} data={msg} variant="postit" />
+          <div
+            key={msg.id}
+            onClick={() => setSelected(msg)}
+            className="cursor-pointer transform hover:scale-105 hover:rotate-1 transition"
+          >
+          <Card data={msg} variant="postit" />
+          </div>
         ))}
       </main>
+      {selected && (
+        <Modal onClose={() => setSelected(null)}>
+          <Card data={selected} variant="single" />
+        </Modal>
+      )}
   </div>
   );
 }
