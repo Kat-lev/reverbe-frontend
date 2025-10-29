f210b29462725268ce0902f806470bb1794bee84
Mariona Cuyàs <cuyas.mariona@egmail.com>
2025-10-29 17:32:55 +0100

    Create postit page

--- src/pages/PostItPage.jsx
+++ src/pages/PostItPage.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Header from "../components/Header";
+import Card from "../components/Card";
+import { mockMessages } from "../mockData";
 
 function PostItPage() {
   return (
-  <>
+  <div className="min-h-screen bg-gray-100">
     <Header />
-    <div>
-      Post-it View (Style 3)
-    </div>
-  </>
+    <main
+        className="
+          max-w-7xl mx-auto 
+          p-4 sm:p-6 lg:p-8 
+          grid gap-6
+          grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
+        "
+      >
+        {mockMessages.slice(0, 6).map((msg) => (
+          <Card key={msg.id} data={msg} variant="postit" />
+        ))}
+      </main>
+  </div>
   );
 }
 
