6a03e5a4e830cc4fb9bad1b11ebacc0cae9965bd
Mariona Cuyàs <cuyas.mariona@egmail.com>
2025-10-29 17:48:57 +0100

    Add overlay click in modal to close it clicking anywhere outside the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import Button from "./Button";
 
 export default function Modal({ children, onClose }) {
+   const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
   return (
-     <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
+     <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50"
+      onClick={handleOverlayClick}>
       <div className="relative bg-white rounded-lg shadow-2xl max-w-3xl w-full p-6">
         <div className="flex justify-end">
           <Button
